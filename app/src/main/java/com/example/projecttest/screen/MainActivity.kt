34d4af3c09f5ccd11f6380f77d7065178af01add
dohdao1 <dohuudao12345@gmail.com>
2025-03-31 14:24:02 +0700

    lam lai

--- app/src/main/java/com/example/projecttest/screen/MainActivity.kt
+++ app/src/main/java/com/example/projecttest/screen/MainActivity.kt
@@ -13,7 +13,6 @@ import com.example.projecttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var workoutProgramAdapter: WorkoutProgramAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
